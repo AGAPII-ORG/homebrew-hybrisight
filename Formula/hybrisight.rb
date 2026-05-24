fc8a38968995b0c62514b1d8b0802667359227df
Shawn <shawnthompson66@gmail.com>
2026-05-24 13:53:16 +0100

    Fix: include Language::Python::Virtualenv

--- Formula/hybrisight.rb
+++ Formula/hybrisight.rb
@@ -1,16 +1,17 @@
 class Hybrisight < Formula
+  include Language::Python::Virtualenv
+
   desc "Deterministic cloud governance assessment CLI for AWS and Azure"
   homepage "https://hybrisight.agapii.org"
   url "https://files.pythonhosted.org/packages/source/h/hybrisight/hybrisight-0.1.2.tar.gz"
   sha256 "1a96cd7677b319700e0b17a5162d5ed10489231348938ed590dfa57fa152bb95"
   license "Apache-2.0"
+  revision 1
 
   depends_on "python@3.12"
 
   def install
-    venv = virtualenv_create(libexec, "python3.12")
-    system libexec/"bin/pip", "install", buildpath
-    bin.install_symlink libexec/"bin/hybrisight"
+    virtualenv_install_with_resources
   end
 
   test do
